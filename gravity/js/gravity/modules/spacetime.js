d22c65e97820be6e43f69243681fa1d4f2b2d030
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2018-07-25 13:48:30 -0400

    Fixing generated stuff's velocity

--- gravity/js/gravity/modules/spacetime.js
+++ gravity/js/gravity/modules/spacetime.js
@@ -147,13 +147,13 @@ define([
 					path: 			path
 				});
 
-				if(spacetime.length < 400) {
+				if(spacetime.length < 1400) {
 					var newObject2 = new objectConstructor({
 						cameraFocus: 	false,
 						x: 				40*Math.sqrt(bnRoot.CoM[0])*Math.random() + bnRoot.CoM[1], // -x+(Math.random()-0.5)*Math.min(mass*5,2000*Math.sqrt(spacetime.length)),
 						y: 				40*Math.sqrt(bnRoot.CoM[0])*Math.random() + bnRoot.CoM[2], // -y+(Math.random()-0.5)*Math.min(mass*5,2000*Math.sqrt(spacetime.length)),
-						velX: 			2*Math.random(), //Math.sqrt(mass)*(Math.random()-0.5)*2.4,
-						velY: 			2*Math.random(), //+Math.sqrt(mass)*(Math.random()-0.5)*2.4,
+						velX: 			2*(Math.random()-0.5), //Math.sqrt(mass)*(Math.random()-0.5)*2.4,
+						velY: 			2*(Math.random()-0.5), //+Math.sqrt(mass)*(Math.random()-0.5)*2.4,
 						accX: 0,
 						accY: 0,
 						mass: 			Math.random()*Math.random()*Math.random()*Math.random()*10, 
