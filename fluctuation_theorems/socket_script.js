4b83a1d7197302476a9cbe6eba94bdd4cc6cb1f4
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-04-21 19:54:20 -0400

    Changing the nature of the initializing and fixing the separation by velocities

--- fluctuation_theorems/socket_script.js
+++ fluctuation_theorems/socket_script.js
@@ -3,9 +3,11 @@ var socket = io("https://evening-springs-71938.herokuapp.com/");
 integralFluctuationAverages = {};
 
 socket.on("calculationResult", function(result) {
-	integralFluctuationAverages['result.velocity'] = integralFluctuationAverages['result.velocity'] || { sum: 0, samples: 0};
+	if(integralFluctuationAverages[result.velocity] == null) {
+		integralFluctuationAverages[result.velocity] = { sum: 0, samples: 0 };
+	}
 	fluctuationAverages = integralFluctuationAverages['result.velocity'];
 	fluctuationAverages.sum += Math.exp(-result.entropy);
 	fluctuationAverages.samples++;
-	console.log("Velocity: " + result.velocity + ", Average: " + fluctuationAverages.sum/samples);
+	console.log("Velocity: " + result.velocity + ", Average: " + fluctuationAverages.sum/fluctuationAverages.samples);
 });
