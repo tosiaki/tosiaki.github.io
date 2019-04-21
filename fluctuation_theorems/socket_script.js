0a553a5aaa49f79c6c4142d19b2eef69b9531f39
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-04-21 19:39:04 -0400

    Calculating the average

--- fluctuation_theorems/socket_script.js
+++ fluctuation_theorems/socket_script.js
@@ -1,5 +1,14 @@
 var socket = io("https://evening-springs-71938.herokuapp.com/");
 
+integralFluctuationAverages = {};
+
+socket.on("newVelocity", function(velocity) {
+	integralFluctuationAverages['result.velocity'] = { sum: 0, samples: 0 };
+});
+
 socket.on("calculationResult", function(result) {
-	console.log("Velocity: " + result.velocity + ", Entropy: " + result.entropy);
+	fluctuationAverages = integralFluctuationAverages['result.velocity'];
+	fluctuationAverages.sum += Math.exp(-result.entropy);
+	fluctuationAverages.samples++;
+	console.log("Velocity: " + result.velocity + ", Average: " + fluctuationAverages.sum/samples);
 });
