2cdd05995e13cb9fa576dbcad60a0bd66a227aad
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-04-21 19:48:07 -0400

    Fixing the initializing of new velocities

--- fluctuation_theorems/socket_script.js
+++ fluctuation_theorems/socket_script.js
@@ -2,11 +2,8 @@ var socket = io("https://evening-springs-71938.herokuapp.com/");
 
 integralFluctuationAverages = {};
 
-socket.on("newVelocity", function(velocity) {
-	integralFluctuationAverages[velocity] = { sum: 0, samples: 0 };
-});
-
 socket.on("calculationResult", function(result) {
+	integralFluctuationAverages['result.velocity'] = integralFluctuationAverages['result.velocity'] || { sum: 0, samples: 0};
 	fluctuationAverages = integralFluctuationAverages['result.velocity'];
 	fluctuationAverages.sum += Math.exp(-result.entropy);
 	fluctuationAverages.samples++;
