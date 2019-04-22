331ed02f4cb507da01fcb417ff4481f0e8543d43
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-04-21 20:07:34 -0400

    Adding a display of samples and averages

--- fluctuation_theorems/socket_script.js
+++ fluctuation_theorems/socket_script.js
@@ -5,9 +5,27 @@ integralFluctuationAverages = {};
 socket.on("calculationResult", function(result) {
 	if(integralFluctuationAverages[result.velocity] == null) {
 		integralFluctuationAverages[result.velocity] = { sum: 0, samples: 0 };
+		var noticeBox = document.createElement('div');
+		noticeBox.setAttribute("id", "box-"+result.velocity);
+		var notice = document.createTextNode('For the velocity of ' + result.velocity + ', the average e^(-Delta S) is currently equal to ');
+		var velocityHolder = document.createElement('div');
+		velocityHolder.setAttribute("class", "velocity");
+		var notice2 = document.createTextNode(' and currently has ');
+		var samplesHolder = document.createElement('div');
+		samplesHolder.setAttribute("class", "samples");
+		var notice3 = document.createTextNode(' samples.');
+		document.body.appendChild(noticeBox);
+		noticeBox.appendChild(notice);
+		noticeBox.appendChild(velocityHolder);
+		noticeBox.appendChild(notice2);
+		noticeBox.appendChild(samplesHolder);
+		noticeBox.appendChild(notice3);
+
 	}
-	fluctuationAverages = integralFluctuationAverages['result.velocity'];
+	fluctuationAverages = integralFluctuationAverages[result.velocity];
 	fluctuationAverages.sum += Math.exp(-result.entropy);
 	fluctuationAverages.samples++;
 	console.log("Velocity: " + result.velocity + ", Average: " + fluctuationAverages.sum/fluctuationAverages.samples);
+	document.getElementById("box-" + result.velocity).getElementsByClassName('velocity')[0].innerHTML = result.velocity;
+	document.getElementById("box-" + result.samples).getElementsByClassName('samples')[0].innerHTML = result.samples;
 });
