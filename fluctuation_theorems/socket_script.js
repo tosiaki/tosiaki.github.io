6d38aceba5bf9b6bbcb205fb01d7daa0e6085cd2
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-04-21 22:41:37 -0400

    Adding proportion of lowerings

--- fluctuation_theorems/socket_script.js
+++ fluctuation_theorems/socket_script.js
@@ -4,7 +4,7 @@ integralFluctuationAverages = {};
 
 socket.on("calculationResult", function(result) {
 	if(integralFluctuationAverages[result.velocity] == null) {
-		integralFluctuationAverages[result.velocity] = { sum: 0, samples: 0, entropies: [] };
+		integralFluctuationAverages[result.velocity] = { sum: 0, samples: 0, entropies: [], entropyLowerings: 0 };
 		var noticeBox = document.createElement('div');
 		noticeBox.setAttribute("id", "box-"+result.velocity);
 		var notice = document.createTextNode('For the velocity of ' + result.velocity + ', the average e^(-Delta S) is currently equal to ');
@@ -13,7 +13,13 @@ socket.on("calculationResult", function(result) {
 		var notice2 = document.createTextNode(' and currently has ');
 		var samplesHolder = document.createElement('span');
 		samplesHolder.setAttribute("class", "samples");
-		var notice3 = document.createTextNode(' samples.');
+		var notice3 = document.createTextNode(' samples. The number of times the entropy has been lowered is ');
+		var loweringsHolder = document.createElement('span');
+		loweringsHolder.setAttribute("class", "lowerings");
+		var notice4 = document.createTextNode(' for a percentage of ');
+		var loweringsProportionHolder = document.createElement('span');
+		loweringsProportionHolder.setAttribute("class", "lowerings-proportion");
+		var notice5 = document.createTextNode('%.');
 		var chart = document.createElement('div');
 		chart.setAttribute("id", "histogram-" + result.velocity);
 
@@ -23,6 +29,10 @@ socket.on("calculationResult", function(result) {
 		noticeBox.appendChild(notice2);
 		noticeBox.appendChild(samplesHolder);
 		noticeBox.appendChild(notice3);
+		noticeBox.appendChild(loweringsHolder);
+		noticeBox.appendChild(notice4);
+		noticeBox.appendChild(loweringsProportionHolder);
+		noticeBox.appendChild(notice5);
 		noticeBox.appendChild(chart);
 	}
 	elementName = "box-" + result.velocity;
@@ -30,10 +40,15 @@ socket.on("calculationResult", function(result) {
 	fluctuationAverages.sum += Math.exp(-result.entropy);
 	fluctuationAverages.entropies.push(result.entropy);
 	fluctuationAverages.samples++;
+	if(result.entropy < 0) {
+		fluctuationAverages.entropyLowerings++;
+	}
 	average = fluctuationAverages.sum/fluctuationAverages.samples;
 	console.log("Velocity: " + result.velocity + ", Average: " + average); 
 	document.getElementById(elementName).getElementsByClassName('integration-fluctionation-average')[0].innerHTML = average;
 	document.getElementById(elementName).getElementsByClassName('samples')[0].innerHTML = fluctuationAverages.samples;
+	document.getElementById(elementName).getElementsByClassName('lowerings')[0].innerHTML = fluctuationAverages.entropyLowerings;
+	document.getElementById(elementName).getElementsByClassName('lowerings-proportion')[0].innerHTML = fluctuationAverages.entropyLowerings/fluctuationAverages.samples;
 	MG.data_graphic({
 		data: fluctuationAverages.entropies,
 		width: 650,
