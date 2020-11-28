302abe8ecc6949f9932e083fa3ba937985735089
Tosiaki 「」 <aworldwithoutsnowflakes1@gmail.com>
2020-11-28 09:40:58 -0500

    Commenting out joining objects.

--- gravity/js/gravity/modules/spacetime.js
+++ gravity/js/gravity/modules/spacetime.js
@@ -804,7 +804,7 @@ define([
 
 				}
 
-				recursivelyJoinClusteringObjects();
+				// recursivelyJoinClusteringObjects();
 
 				// ----------------------------------------
 				// | Newtons law of universal gravitation |
